7f7d9d62123119b47e205af651fceb45a1781198
Javier Antonio Gonzalez Trejo <jglezt.ranger@gmail.com>
2026-05-02 16:39:46 -0600

    fix: Remove unintended changes after merges from main branch

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -30,9 +30,6 @@
 (when (maybe-require-package 'toml-mode)
   (add-to-list 'auto-mode-alist '("\\(poetry\\|uv\\)\\.lock\\'" . toml-mode)))
 
-(require-package 'pyvenv)
-(require 'pyvenv)
-
 (when (maybe-require-package 'reformatter)
   (reformatter-define black :program "black" :args '("-")))
 
@@ -40,58 +37,6 @@
   (add-to-list 'project-vc-extra-root-markers "pyproject.toml"))
 (with-eval-after-load 'projectile
   (add-to-list 'projectile-project-root-files "pyproject.toml"))
-
-;;; lsp-mode setup
-(use-package lsp-mode
-  :demand t
-  :config
-  (defun md/lsp-setup()
-    ;; recommended by LSP docs for performance
-    (setq read-process-output-max (* 1024 1024)) ;; 1mb
-
-    (lsp-enable-imenu)
-    (setq
-     lsp-auto-configure t
-     lsp-enable-dap-auto-configure nil ; Don't try to auto-enable dap: this creates a lot of binding clashes
-     lsp-auto-guess-root t ; Uses projectile to guess the project root.
-     lsp-before-save-edits t
-     lsp-eldoc-enable-hover t
-     lsp-eldoc-render-all nil
-     lsp-completion-enable t
-     lsp-completion-show-detail t
-     lsp-completion-show-kind t
-     lsp-enable-file-watchers t
-     lsp-file-watch-threshold 100
-     lsp-enable-folding t
-     lsp-enable-imenu t
-     lsp-enable-indentation t
-     lsp-enable-links t
-     lsp-enable-on-type-formatting nil
-     lsp-enable-snippet nil ;; Not supported by company capf, which is the recommended company backend
-     lsp-enable-symbol-highlighting nil
-     lsp-enable-text-document-color nil
-     lsp-enable-xref t
-     lsp-flycheck-live-reporting nil
-     lsp-idle-delay 0.5
-     lsp-imenu-show-container-name t
-     lsp-imenu-sort-methods '(position kind name)
-     lsp-signature-auto-activate t
-     lsp-signature-render-documentation t
-     lsp-signature-doc-lines 10))
-  :hook
-  ((python-mode . lsp)))
-
-(use-package lsp-ui
-  :config (setq lsp-ui-sideline-show-hover t
-                lsp-ui-sideline-delay 0.5
-                lsp-ui-doc-delay 5
-                lsp-ui-sideline-ignore-duplicates t
-                lsp-ui-doc-position 'bottom
-                lsp-ui-doc-alignment 'frame
-                lsp-ui-doc-header nil
-                lsp-ui-doc-include-signature t
-                lsp-ui-doc-use-childframe t)
-  :commands lsp-ui-mode)
 
 (provide 'init-python)
 ;;; init-python.el ends here
